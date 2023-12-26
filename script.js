c222dccf0600dea1d91aef876510d70d357da68e
Roger Rosset <rrosset91@gmail.com>
2023-12-26 15:01:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const ballSize = 10;
 let gameStarted = false;
 let ballColor = "#f00";
 let balls = [];
-const initialSpeed = 1;
+const initialSpeed = 0.5;
 const angleVariation = 1.1;
 
 function Ball ( x, y, speedX, speedY, color ) {
